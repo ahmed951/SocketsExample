aa6162809b3bb1f678cd5f81dfbc99fc1a1a3b1e
Ahmed Alaa <42003517+ahmed951@users.noreply.github.com>
2021-11-02 20:33:33 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-javac  'src/main/java/com/mycompany/assignment1/Server.java' 'src/main/java/com/mycompany/assignment1/Computer.java' 'src/main/java/com/mycompany/assignment1/Sensors.java' 'src/main/java/com/mycompany/assignment1/SensorsMultiThreaded.java' 'src/main/java/com/mycompany/assignment1/ServerMultiThreaded.java' 
-
-
-$SHELL
